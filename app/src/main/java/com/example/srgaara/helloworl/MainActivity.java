4dd6e65e17931453926e74bb0b5bde0735da76a6
SrGaara <nikel_black@hotmail.com>
2017-08-10 20:37:23 -0500

    onclick listener and text

--- app/src/main/java/com/example/srgaara/helloworl/MainActivity.java
+++ app/src/main/java/com/example/srgaara/helloworl/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.srgaara.helloworl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickButton(View view){
+
+        Log.v("Demo","Button");
+
+    }
 }
-gi
